9858b0ed86192e39683fd8f5a5534da1419a55a8
Vkrtn <v.khqritknove.000@gmail.com>
2023-10-24 20:17:32 +0300

    first commit

---
+++ js/slick.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.hero-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: false,
+      });
+  });
+
+  
